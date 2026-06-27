df27e11b1202a4f091df9b7756fb1909b7b1de33
Sulav Gurung <sulavgrg09@email.com>
2026-06-27 19:20:35 +0545

    Created backend UNIX socket

--- Privilege-Separation/Backend.c
+++ Privilege-Separation/Backend.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("Backend started.\n");
+    int server_socket;
+
+    server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (server_socket == -1)
+    {
+        printf("Socket creation failed.\n");
+        return 1;
+    }
+
+    printf("Socket created successfully.\n");
+
+    close(server_socket);
+
     return 0;
 }
