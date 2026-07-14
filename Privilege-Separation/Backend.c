5bc45b373cde93949ef3d92e5e29241386c509bc
Sulav Gurung <sulavgrg09@email.com>
2026-07-14 14:24:16 +0545

    Implemented password verification

--- Privilege-Separation/Backend.c
+++ Privilege-Separation/Backend.c
@@ -11,7 +11,8 @@ int main()
 
     struct sockaddr_un server_address;
 
-    // Create a UNIX domain socket
+    char password[100];
+
     server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
 
     if (server_socket == -1)
@@ -20,21 +21,17 @@ int main()
         return 1;
     }
 
-    // Set the socket family and socket path
     server_address.sun_family = AF_UNIX;
     strcpy(server_address.sun_path, "/tmp/auth_socket");
 
-    // Remove the old socket file if it already exists
     unlink("/tmp/auth_socket");
 
-    // Bind the socket to the file path
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
     {
         printf("Bind failed.\n");
         return 1;
     }
 
-    // Start listening for incoming connections
     if (listen(server_socket, 5) == -1)
     {
         printf("Listen failed.\n");
@@ -43,7 +40,6 @@ int main()
 
     printf("Backend is waiting for a connection...\n");
 
-    // Accept a connection from the frontend
     client_socket = accept(server_socket, NULL, NULL);
 
     if (client_socket == -1)
@@ -54,10 +50,11 @@ int main()
 
     printf("Frontend connected successfully.\n");
 
-    // Close the client socket
-    close(client_socket);
+    read(client_socket, password, sizeof(password));
 
-    // Close the server socket
+    printf("Received password: %s\n", password);
+
+    close(client_socket);
     close(server_socket);
 
     return 0;
