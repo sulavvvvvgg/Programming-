9d7ad13d0f120311cd8e843c337b01b29f3ad782
Sulav Gurung <sulavgrg09@email.com>
2026-07-14 14:22:20 +0545

    Implemented password transfer using UNIX socket

--- Privilege-Separation/Frontend.c
+++ Privilege-Separation/Frontend.c
@@ -6,12 +6,39 @@
 
 int main()
 {
+    int client_socket;
+
+    struct sockaddr_un server_address;
+
     char password[100];
+
+    client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (client_socket == -1)
+    {
+        printf("Socket creation failed.\n");
+        return 1;
+    }
+
+    server_address.sun_family = AF_UNIX;
+    strcpy(server_address.sun_path, "/tmp/auth_socket");
+
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        printf("Connection failed.\n");
+        return 1;
+    }
+
+    printf("Connected to backend successfully.\n");
 
     printf("Enter password: ");
     scanf("%99s", password);
 
-    printf("You entered: %s\n", password);
+    write(client_socket, password, sizeof(password));
+
+    printf("Password sent to backend.\n");
+
+    close(client_socket);
 
     return 0;
 }
